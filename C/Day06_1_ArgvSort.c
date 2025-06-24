5a8e9e0f38892c7bdf671f4668ca5431463ab2be
Shindanai R. <ohmmygod@outlook.co.th>
2025-06-24 23:10:38 +0700

    Tue, 24th June 2025 - 11:10 PM, structure of the program

---
+++ C/Day06_1_ArgvSort.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_sort(char *str)
+{
+	
+}
+
+int	main(int argc, char **argv)
+{
+
+}
