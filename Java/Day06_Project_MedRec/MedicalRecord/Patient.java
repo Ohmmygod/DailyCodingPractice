3c0a7a57cf31bc8891782707706de7309d24ca98
Shindanai R. <ohmmygod@outlook.co.th>
2025-07-11 19:44:49 +0700

    Fri, 11th July 2025 - 7:44 PM, starting with Patient Class.

--- Java/Day06_Project_MedRec/MedicalRecord/Patient.java
+++ Java/Day06_Project_MedRec/MedicalRecord/Patient.java
@@ -1,1 +1,13 @@
+import java.time.LocalDate;
 
+class Patient {
+	protected final int HN;
+	protected String fName;
+	protected String lName;
+	protected LocalDate dateOfBirth;
+	protected final String hashedNationalID;
+	protected String address;
+	protected String dial;
+
+	
+}
