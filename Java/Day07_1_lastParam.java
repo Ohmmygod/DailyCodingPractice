3e8ddc2070a74ee5be10ccd843511312445273ad
Shindanai R. <ohmmygod@outlook.co.th>
2025-07-14 22:07:08 +0700

    Day 7 - basic parameter

---
+++ Java/Day07_1_lastParam.java
@@ -0,0 +1,13 @@
+public class Day07_1_lastParam.java {
+	public static void main(String[] args) {
+		if (args.length < 1)
+		{
+			System.out.println("\n");
+			return ;
+		}
+
+		System.out.println(args[args.length - 1]);
+		return ;
+	}
+}
+
