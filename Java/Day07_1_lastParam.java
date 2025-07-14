1a225222359094e4f3e6f5c02bc1c49eb828491c
Shindanai R. <ohmmygod@outlook.co.th>
2025-07-14 22:11:47 +0700

    Day7_1 - final version

--- Java/Day07_1_lastParam.java
+++ Java/Day07_1_lastParam.java
@@ -1,13 +1,13 @@
 public class Day07_1_lastParam {
 	public static void main(String[] args) {
 		if (args.length < 1)
-		{
-			System.out.println("\n");
-			return ;
-		}
+			System.out.println();
 
+		else
+		{
 		System.out.println(args[args.length - 1]);
 		return ;
+		}
 	}
 }
 
